cba8ce47a15af9368c3bd76933d3b3414bf84103
MoonSolid <rnreo20@gmail.com>
2019-12-18 20:31:55 +0900

    05

--- src/main/java/com/moonsolid/sc/App.java
+++ src/main/java/com/moonsolid/sc/App.java
@@ -4,11 +4,9 @@
 package com.moonsolid.sc;
 
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
-    }
+  public static void main(String[] args) {
+    System.out.println("오늘도 안녕하세요!");
+  
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
     }
 }
