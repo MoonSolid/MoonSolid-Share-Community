ee28a0deb58d2d003112715d965126f504caadeb
MoonSolid <rnreo20@gmail.com>
2019-12-18 16:04:03 +0900

    프로젝트 시작

---
+++ src/main/java/com/moonsolid/sc/App.java
@@ -0,0 +1,11 @@
+//프로젝트:하루를 공유하는 홈페이지
+package com.moonsolid.sc;
+public class App {
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("오늘도 안녕하세요 !");
+    }
+}
